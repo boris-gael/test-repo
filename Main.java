e9d2054e5f24daf5d0203bc1851287d0881872b4
softeam2 <softeam2@FRP02065.softeam.com>
2022-11-12 19:38:51 +0100

    main change

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("My main class.");
+        System.out.println("My main classe.");
         System.out.println("1+2=3");
         System.out.println("1/2=0.5");
     }
